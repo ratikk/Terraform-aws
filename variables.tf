efbedf38d7ce943b8101d5f0b62fbd67dcea7be4
Kinshuk Makkad <kmakkad@deloitte.com>
2023-08-07 13:13:29 -0500

    Tagging Example

---
+++ variables.tf
@@ -0,0 +1,17 @@
+locals {
+    pcm_tags  {
+        COST = ""
+        SERVICES = ""
+        CLOUD_ARCH = ""
+        COMPLIANCE_STD = ""
+        SECURITY = ""
+    }
+
+    hhs_tags {
+        AGENCY = "HHS"
+        ENV = "PROD"
+        APP_NAME = "TIERS"
+        ZONE_A = "A"
+        ZONE_B = "B"
+    }
+}
